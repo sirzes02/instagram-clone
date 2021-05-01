51904f1d495c38105c8dbaae6051f46885d850b0
sirzes02 <sirzes02@gmail.com>
2021-05-01 18:40:09 -0500

    feed

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,30 +1,63 @@
 import React, { useState, useEffect } from 'react';
 import { FlatList, View } from 'react-native';
 
-import { Post, Header, Avatar, Name, PostImage, Description } from './styles';
+import {
+  Post,
+  Header,
+  Avatar,
+  Name,
+  PostImage,
+  Description,
+  Loading,
+} from './styles';
 
 const Feed = () => {
   const [feed, setFeed] = useState([]);
+  const [page, setPage] = useState(1);
+  const [total, setTotal] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [refreshing, setRefreshing] = useState(false);
+
+  async function loadPage(pageNumber = page, shouldRefresh = false) {
+    if (total && pageNumber > total) return;
+
+    setLoading(true);
+
+    const response = await fetch(
+      `http://localhost:3000/feed?_expand=author&_limit=5&_page=${pageNumber}`,
+    );
+
+    const data = await response.json();
+    const totalItems = response.headers.get('X-Total-Count');
+
+    setTotal(Math.floor(totalItems / 5));
+    setFeed(shouldRefresh ? data : [...feed, ...data]);
+    setPage(pageNumber + 1);
+    setLoading(false);
+  }
 
   useEffect(() => {
-    async function loadFeed() {
-      const response = await fetch(
-        'http://localhost:3000/feed?_expand=author&_limit=5&_page=1',
-      );
-
-      const data = await response.json();
-
-      setFeed(data);
-    }
-
-    loadFeed();
+    loadPage();
   }, []);
+
+  async function refreshList() {
+    setRefreshing(true);
+
+    await loadPage(1, true);
+
+    setRefreshing(false);
+  }
 
   return (
     <View>
       <FlatList
         data={feed}
         keyExtractor={post => String(post.id)}
+        onEndReached={() => loadPage()}
+        onEndReachedThreshold={0.1}
+        onRefresh={refreshList}
+        refreshing={refreshing}
+        ListFooterComponent={loading && <Loading />}
         renderItem={({ item }) => (
           <Post>
             <Header>
@@ -32,7 +65,8 @@ const Feed = () => {
               <Name>{item.author.name}</Name>
             </Header>
 
-            <PostImage source={{ uri: item.image }} />
+            <PostImage ratio={item.aspectRatio} source={{ uri: item.image }} />
+
             <Description>
               <Name>{item.author.name}</Name> {item.description}
             </Description>
